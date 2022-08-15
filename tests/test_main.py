3b0d0a57ebd1be2502659944b7b6013017c7b4cf
Oleksii Proshchenko <80070761+MasterpieceElbow@users.noreply.github.com>
2022-08-15 10:07:34 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,13 +4,12 @@ from contextlib import redirect_stdout
 from io import StringIO
 from unittest.mock import MagicMock
 
-
 from app.main import shop_trip
 
 
 def test_shop_trip_output(monkeypatch):
     datetime_mock = MagicMock(wrap=datetime.datetime)
-    datetime_mock.now.return_value = datetime.datetime(2020, 3, 11, 13, 15, 34)
+    datetime_mock.now.return_value = datetime.datetime(2021, 1, 4, 12, 33, 41)
     monkeypatch.setattr(datetime, "datetime", datetime_mock)
 
     f = StringIO()
@@ -25,7 +24,7 @@ Bob's trip to the Shop '24/7' costs 31.48
 Bob's trip to the Central Shop costs 39.28
 Bob rides to Outskirts Shop
 
-Date: 11/03/2020 13:15:34
+Date: 04/01/2021 12:33:41
 Thanks, Bob, for you purchase!
 You have bought: 
 4 milks for 12 dollars
@@ -43,7 +42,7 @@ Alex's trip to the Shop '24/7' costs 15.95
 Alex's trip to the Central Shop costs 17.98
 Alex rides to Shop '24/7'
 
-Date: 11/03/2020 13:15:34
+Date: 04/01/2021 12:33:41
 Thanks, Alex, for you purchase!
 You have bought: 
 2 milks for 4 dollars
